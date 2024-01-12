04d6dd026814fbe84a4e087e3d9a2a1f21ecd8bc
Laptop User <Laptop User@DESKTOP-R38QT4B>
2024-01-11 20:13:30 -0800

    My First commit

---
+++ src/sample/Test.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Github");
+	}
+
+}
